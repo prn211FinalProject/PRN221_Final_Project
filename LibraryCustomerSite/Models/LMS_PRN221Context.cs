fea77d048326d002e2a15a925cbc7e3a06a0e525
ttmbg2003 <trantuanminhbg2003@gmail.com>
2024-11-08 02:21:55 +0700

    pic

--- LibraryCustomerSite/Models/LMS_PRN221Context.cs
+++ LibraryCustomerSite/Models/LMS_PRN221Context.cs
@@ -33,7 +33,7 @@ namespace LibraryCustomerSite.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source=HUNGNGO\\HUNGNGO;Initial Catalog=LMS_PRN221; Trusted_Connection=SSPI;Encrypt=false;TrustServerCertificate=true");
+                optionsBuilder.UseSqlServer("Data Source=MINH-PC\\MINHPC;Initial Catalog=LMS_PRN221; Trusted_Connection=SSPI;Encrypt=false;TrustServerCertificate=true");
             }
         }
 
